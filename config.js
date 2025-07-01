0d944a0aa9a87052e889935e8e38ea3ffefb4604
kingjoker1234 <kingjoker1239381@gmail.com>
2025-07-01 17:42:53 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,4 +6,6 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
+ALIVE_IMG: process.env.AlIVE_IMG || "https://www.freepik.com/free-photos-vectors/joker-card",
+ALIVE_MSG: process.env.ALIVE_MSG || "Hello I am king joker bot I am alive now",
 };
